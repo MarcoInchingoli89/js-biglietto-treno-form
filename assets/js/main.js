f3d4b10c3bebfef95a0f76e7626e0ef5128782b1
Marco Inchingoli <evilmarkus@hotmail.it>
2022-10-08 01:08:45 +0200

    Final Commit Fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,14 +2,6 @@
 
 /* Costanti */
 
-// Nome del passeggero
-const userName = document.getElementById("userName").value;
-// Chilometri percorsi
-const kmTraveled = document.getElementById("km").value;
-// Età del passeggero
-const passengerAge = document.getElementById("age").value;
-// Prezzo in base ai km (0.21 € al km)
-const ticketPrice = (kmTraveled * 0.21);
 // Sconto del 20%
 const underageDiscount = 0.2
 // Sconto del 40%
@@ -22,22 +14,27 @@ const priceGenerator = document.querySelector('button');
 // Impostare una funzione al nostro pulsante
 priceGenerator.addEventListener('click', function() {
 
-    /* console.log('Ho cliccato sul pulsante'); */
+    // Nome e congnome dell'utente
+    const userName = document.getElementById("userName").value;
     console.log('Nome e Cognome:', userName);
     document.getElementById("passenger_name").innerHTML = userName;
+    // Chilometri che si vuole percorrere
+    const kmTraveled = document.getElementById("km").value;
     console.log('Chilometri percorsi:', `${kmTraveled} km`);
+    // Età del passeggero
+    const passengerAge = document.getElementById("age").value;
     console.log('Età del passeggero:', passengerAge);
     // Se il passeggero ha meno di 18 anni applica il 20% di sconto
     if (passengerAge < 18) {
-        const underageTicketPrice = (ticketPrice - underageDiscount);
+        const underageTicketPrice = ((kmTraveled * 0.21) - underageDiscount);
         document.getElementById("ticket_price").innerHTML = (new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR' }).format(underageTicketPrice));
     // Se il passeggero ha più di 65 anni applica il 40% di sconto
     } else if (passengerAge > 65) {
-        const over65TicketPrice = (ticketPrice - over65Discount);
+        const over65TicketPrice = ((kmTraveled * 0.21) - over65Discount);
         document.getElementById("ticket_price").innerHTML = (new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR' }).format(over65TicketPrice));
     // Altrimenti non applicare nessuno sconto
     } else {
-        document.getElementById("ticket_price").innerHTML = (new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR' }).format(ticketPrice));
+        document.getElementById("ticket_price").innerHTML = (new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR' }).format(kmTraveled * 0.21));
     }
 
 })
