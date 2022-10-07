e721894c776579e0bb2f226de548e0c1a8523d11
Marco Inchingoli <evilmarkus@hotmail.it>
2022-10-07 23:41:44 +0200

    Final Commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,5 @@
 /* Calcolo costo biglietto del treno */
 
-/* Milestone 1 */
-
-/* Iniziamo implementando il programma senza alcuna estetica: usando esclusivamente due input e un bottone (non stilizzati), realizziamo le specifiche scritte sopra.
-La risposta finale (o output) sarà anch’essa da scrivere in console. */
-
 /* Costanti */
 
 // Nome del passeggero
@@ -27,27 +22,22 @@ const priceGenerator = document.querySelector('button');
 // Impostare una funzione al nostro pulsante
 priceGenerator.addEventListener('click', function() {
 
-    console.log('Ho cliccato sul pulsante');
+    /* console.log('Ho cliccato sul pulsante'); */
     console.log('Nome e Cognome:', userName);
+    document.getElementById("passenger_name").innerHTML = userName;
     console.log('Chilometri percorsi:', `${kmTraveled} km`);
     console.log('Età del passeggero:', passengerAge);
     // Se il passeggero ha meno di 18 anni applica il 20% di sconto
     if (passengerAge < 18) {
         const underageTicketPrice = (ticketPrice - underageDiscount);
-        console.log(new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR' }).format(underageTicketPrice));
+        document.getElementById("ticket_price").innerHTML = (new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR' }).format(underageTicketPrice));
     // Se il passeggero ha più di 65 anni applica il 40% di sconto
     } else if (passengerAge > 65) {
         const over65TicketPrice = (ticketPrice - over65Discount);
-        console.log(new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR' }).format(over65TicketPrice));
+        document.getElementById("ticket_price").innerHTML = (new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR' }).format(over65TicketPrice));
     // Altrimenti non applicare nessuno sconto
     } else {
-        console.log(new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR' }).format(ticketPrice));
+        document.getElementById("ticket_price").innerHTML = (new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR' }).format(ticketPrice));
     }
 
 })
-
-/* Milestone 2 */
-
-/* Solo una volta che il milestone 1 sarà completo e funzionante allora realizzeremo un form in pagina in cui l’utente potrà inserire i dati e visualizzare il calcolo finale con il prezzo.
-Il riepilogo dei dati e l'output del prezzo finale, andranno quindi stampati in pagina (il prezzo dovrà essere formattato con massimo due decimali, per indicare i centesimi sul prezzo).
-Questo richiederà un minimo di ricerca. */
