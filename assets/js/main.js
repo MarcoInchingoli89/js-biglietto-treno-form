9ef25af045c742d7dff5727e2f4f0d4575f88d50
Marco Inchingoli <evilmarkus@hotmail.it>
2022-10-07 16:40:40 +0200

    Add code in the button

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,6 +5,18 @@
 /* Iniziamo implementando il programma senza alcuna estetica: usando esclusivamente due input e un bottone (non stilizzati), realizziamo le specifiche scritte sopra.
 La risposta finale (o output) sarà anch’essa da scrivere in console. */
 
+/* Costanti */
+
+// Chilometri percorsi
+const kmTraveled = document.getElementById("km");
+// Età del passeggero
+const passengerAge = document.getElementById("age");
+// Prezzo in base ai km (0.21 € al km)
+const ticketPrice = (kmTraveled * 0.21);
+// Sconto del 20%
+const underageDiscount = 0.2
+// Sconto del 40%
+const over65Discount = 0.4
 
 /* Calcolo prezzo biglietto */
 
@@ -19,6 +31,19 @@ priceGenerator.addEventListener('click', function() {
     console.log('Chilometri percorsi:', kmTraveled.value);
     const passengerAge = document.getElementById("age");
     console.log('Età passeggero:', passengerAge.value);
+    if (passengerAge < 18) {
+        const underageTicketPrice = ((kmTraveled * 0.21) - underageDiscount);
+        console.log(new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR' }).format(underageTicketPrice));
+    // Se il passeggero ha più di 65 anni applica il 40% di sconto
+    } else if (passengerAge > 65) {
+        /* const over65Discount = ((passengerAge * 40) /100); */
+        const over65TicketPrice = (ticketPrice - over65Discount);
+        console.log(new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR' }).format(over65TicketPrice));
+    // Altrimenti non applicare nessuno sconto
+    } else {
+        console.log(new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR' }).format(ticketPrice));
+    }
+
 
 
 
